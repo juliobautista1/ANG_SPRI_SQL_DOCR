0d8303762c3bd5bbb67c8501b292c0eff88eb838
juliobautista1 <121524350+juliobautista1@users.noreply.github.com>
2024-11-26 13:55:09 -0600

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test de Aplicación', () => {
   it('debería cargar la página principal', () => {
-    cy.visit('/');
+    cy.visit('http://127.0.0.1:4200/');
     cy.contains('Bienvenido'); // Ajusta este texto según lo que esperes en tu página
   });
 });
