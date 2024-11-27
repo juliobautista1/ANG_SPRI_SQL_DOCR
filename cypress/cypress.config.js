0bccf6f00aabeabd2b5875980c0dac9339be5791
juliobautista1 <121524350+juliobautista1@users.noreply.github.com>
2024-11-27 12:07:30 -0600

    Update cypress.config.js

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,6 +3,7 @@ module.exports = {
     e2e: {
       baseUrl: 'http://127.0.0.1:4200',  // Asegúrate de que esté apuntando a tu frontend
       specPattern: 'cypress/e2e/*.cy.js',  // Cambia el patrón a tus pruebas
+      supportFile: false,  // Desactiva el archivo de soporte
       },
   };
   
