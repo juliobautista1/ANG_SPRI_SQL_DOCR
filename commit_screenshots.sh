6f889996eceea6ed55ec6405673b2f5ff28c2a30
juliobautista1 <121524350+juliobautista1@users.noreply.github.com>
2024-12-02 17:50:14 -0600

    Create commit_screenshots.sh

---
+++ commit_screenshots.sh
@@ -0,0 +1,15 @@
+# Asegúrate de que las capturas de pantalla existan
+if [ -d "/e2e/cypress/screenshots" ] && [ "$(ls -A /e2e/cypress/screenshots)" ]; then
+  echo "Capturas de pantalla encontradas. Procediendo con commit."
+
+  # Añade las capturas de pantalla al repositorio
+  git add cypress/screenshots
+
+  # Realiza el commit si hay algo nuevo
+  git commit -m "Agrega capturas de pantalla de Cypress"
+  
+  # Realiza el push al repositorio
+  git push
+else
+  echo "No se encontraron capturas de pantalla. No se realizará commit."
+fi
